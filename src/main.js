3c0384e64507a5faed6b3265cfb200ccfc1af55c
smtanbin <mds.tanbin@gmail.com>
2022-01-23 18:16:40 +0600

    start

---
+++ src/main.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(__dirname + '/public'))
+app.set('views', path.join(__dirname, '/views'))
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+// use res.render to load up an ejs view file
+
+/** 
+ **Routes**
+ **/
+// index page
+app.get('/', function(req, res) {
+	res.locals = {
+		title: 'Home'
+	}
+	res.render('index')
+})
+app.get('/login', function(req, res) {
+	res.locals = {
+		title: 'Login'
+	}
+	res.render('pages/login')
+})
+
+// registration
+app.get('/reg', function(req, res) {
+	res.render('pages/registration')
+})
+
+const port = 80
+app.listen(port)
+console.log('Server is listening on port ' + port)
