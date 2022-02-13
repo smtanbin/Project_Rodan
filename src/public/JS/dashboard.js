651286223b3b1f8f4d23f7b41a9bc3debad031e8
smtanbin <mds.tanbin@gmail.com>
2022-02-13 15:05:42 +0600

    dash

--- src/public/JS/dashboard.js
+++ src/public/JS/dashboard.js
@@ -20,6 +20,13 @@ const timeline = async () => {
 	}
 
 	await fetch(url, requestOptions).then((response) => response.json()).then((payload) => {
+		if (payload === null){
+			document.getElementById('timeline').innerHTML += `
+			<tr>
+			<td colspan="14"> No Data Found!</td>
+			
+			</tr>`
+		} else{
 		payload.map((data) => {
 			const {
 				TRANS_NO,
@@ -41,20 +48,22 @@ const timeline = async () => {
 			document.getElementById('timeline').innerHTML += `
 			<tr>
 			<td class="text-tiny">${new Date(TRANS_DATE).toDateString()}</td>
-			<td class="text-tiny">${TRANS_NO}</td>
-			<td class="text-tiny">${TRANS_FROM}</td>
-			<td class="text-tiny">${FROM_GL}</td>
-			<td class="text-tiny">${TRANS_TO}</td>
-			<td class="text-tiny">${TO_GL}</td>
+			<td class="text-tiny text-primary">${TRANS_NO}</td>
+			<td class="text-tiny">${CODE}</td>
+			<td class="text-tiny" py-1>${TRANS_FROM}</td>
+			<td class="text-tiny" py-1>${FROM_GL}</td>
+			<td class="text-tiny" py-1>${TRANS_TO}</td>
+			<td class="text-tiny" py-1>${TO_GL}</td>
 			<td class="text-tiny">${PAY_AMT}</td>
-			<td class="text-tiny">${MSG_AMT}</td>
-			<td class="text-tiny">${SCHARGE_AMT}</td>
+			<td class="text-tiny text-success">${MSG_AMT}</td>
+			<td class="text-tiny text-warning">${SCHARGE_AMT}</td>
 			<td class="text-tiny">${MERCHANT_SNAME}</td>
 			<td class="text-tiny">${REF_PHONE}</td>
 			<td class="text-tiny">${NOTE}</td>
 			<td class="text-tiny">${PARTICULAR}</td>
 			</tr>`
 		})
+	}
 	})
 document.getElementById('loading').remove()
 }
